4ebef39a7f0bff6f3f0f60d8ccf3843cb8525345
Runji Wang <wangrunji0408@163.com>
2022-05-21 17:58:24 +0800

    stream: `use madsim::*` -> `use tokio::*`
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/stream/src/task/stream_manager.rs
+++ src/stream/src/task/stream_manager.rs
@@ -18,7 +18,6 @@ use std::sync::Arc;
 use futures::channel::mpsc::{channel, Receiver};
 use itertools::Itertools;
 use madsim::collections::{HashMap, HashSet};
-use madsim::task::JoinHandle;
 use parking_lot::Mutex;
 use risingwave_common::config::StreamingConfig;
 use risingwave_common::error::{ErrorCode, Result, RwError};
@@ -31,6 +30,7 @@ use risingwave_pb::{stream_plan, stream_service};
 use risingwave_rpc_client::ComputeClientPool;
 use risingwave_storage::{dispatch_state_store, StateStore, StateStoreImpl};
 use tokio::sync::oneshot;
+use tokio::task::JoinHandle;
 
 use super::{unique_executor_id, unique_operator_id, CollectResult};
 use crate::executor::dispatch::*;
@@ -549,7 +549,7 @@ impl LocalStreamManagerCore {
 
                         let pool = self.compute_client_pool.clone();
 
-                        madsim::task::spawn(async move {
+                        tokio::spawn(async move {
                             let init_client = async move {
                                 let remote_input = RemoteInput::create(
                                     pool.get_client_for_addr(upstream_addr).await?,
@@ -595,7 +595,7 @@ impl LocalStreamManagerCore {
             let actor = Actor::new(dispatcher, actor_id, self.context.clone());
             self.handles.insert(
                 actor_id,
-                madsim::task::spawn(async move {
+                tokio::spawn(async move {
                     // unwrap the actor result to panic on error
                     actor.run().await.expect("actor failed");
                 }),
@@ -643,7 +643,7 @@ impl LocalStreamManagerCore {
     /// `drop_actor` is invoked by meta node via RPC once the stop barrier arrives at the
     /// sink. All the actors in the actors should stop themselves before this method is invoked.
     fn drop_actor(&mut self, actor_id: ActorId) {
-        let mut handle = self.handles.remove(&actor_id).unwrap();
+        let handle = self.handles.remove(&actor_id).unwrap();
         self.context.retain(|&(up_id, _)| up_id != actor_id);
 
         self.actor_infos.remove(&actor_id);
@@ -655,7 +655,7 @@ impl LocalStreamManagerCore {
     /// `drop_all_actors` is invoked by meta node via RPC once the stop barrier arrives at all the
     /// sink. All the actors in the actors should stop themselves before this method is invoked.
     fn drop_all_actors(&mut self) {
-        for (actor_id, mut handle) in self.handles.drain() {
+        for (actor_id, handle) in self.handles.drain() {
             self.context.retain(|&(up_id, _)| up_id != actor_id);
             self.actors.remove(&actor_id);
             // Task should have already stopped when this method is invoked.
